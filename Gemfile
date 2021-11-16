5cd83ce3dea9145d781e2ab5dbf36de4e0b332ed
Haji Mammadov <hmammado@gmail.com>
2021-11-16 12:59:56 +0100

    Almost done with Index Page for Dogs

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.7.4'
 #Added by me
 
 gem 'devise'
+gem 'faker'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.0.4', '>= 6.0.4.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+
+User.create!(
+  first_name: 'Haji',
+  last_name: 'Mammadov',
+  address: Faker::Address.street_address,
+  email: 'hmammado@gmail.com',
+  password: 'password'
+)
+
+User.create!(
+  first_name: 'Monica',
+  last_name: 'Suy',
+  address: Faker::Address.street_address,
+  email: 'monicasuy96@gmail.com',
+  password: 'password'
+)
+
+User.create!(
+  first_name: 'Diana',
+  last_name: 'Barroso',
+  address: Faker::Address.street_address,
+  email: 'dianabarrg@gmail.com',
+  password: 'password'
+)
+
+10.times do
+  new_user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_address,
+    email: Faker::Internet.email,
+    password: 'password'
+  )
+
+  Dog.create!(
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    size: Faker::Creature::Dog.size,
+    age: Faker::Number.between(from: 1, to: 16),
+    description: Faker::Creature::Dog.meme_phrase,
+    user: new_user
+  )
+end
