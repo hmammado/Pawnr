6f457717205edc74dbef21614abd293e9faccf93
monica suy <monicasuy96@gmail.com>
2021-11-17 11:31:40 +0100

    half of the setup for the mapbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_154202) do
+ActiveRecord::Schema.define(version: 2021_11_17_102216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2021_11_16_154202) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
