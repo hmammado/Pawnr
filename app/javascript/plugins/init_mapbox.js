31948029d987b225e0dc3e1e6e6a7d35a86ad6b2
monica suy <monicasuy96@gmail.com>
2021-11-18 15:35:34 +0100

    added info card to the map markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -7,9 +7,19 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
   };
 
+  const addMarkersToMap = (map, markers) => {
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup) // add this
+        .addTo(map);
+    });
+  };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -30,8 +40,10 @@ const initMapbox = () => {
         .addTo(map);
     });
 
+  addMarkersToMap(map, markers);
   fitMapToMarkers(map, markers);
   }
 };
+
 
 export { initMapbox };
