18c4473d846e4218af61cf1dea2a0b4644e661a3
Haji Mammadov <hmammado@gmail.com>
2021-11-19 11:06:38 +0100

    Updated models

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,7 @@
 class Dog < ApplicationRecord
   belongs_to :user
   has_many :reviews, through: :bookings
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many_attached :photos
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
